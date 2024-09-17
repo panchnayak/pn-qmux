5c1ba9e95a3dce9065f5189b0f47a26476d6be57
panchnayak <panchaleswar@outlook.com>
2024-09-16 22:08:09 -0400

    first commit

---
+++ golang/go.mod
@@ -0,0 +1,5 @@
+module github.com/panchnayak/pn-qmux/golang
+
+go 1.22.2
+
+require golang.org/x/net v0.29.0
---
+++ typescript/codec/index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+export * from "./message.ts";
+// @ts-ignore
+export * from "./encoder.ts";
+// @ts-ignore
+export * from "./decoder.ts";
